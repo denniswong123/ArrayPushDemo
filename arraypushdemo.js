f4b27f2aec6cd70b4319d6d06adb1be299228604
denniswong123 <42422324+denniswong123@users.noreply.github.com>
2018-08-15 12:21:07 -1000

    first commit

---
+++ arraypushdemo.js
@@ -0,0 +1,10 @@
+var firstArr = [];
+var lastArr = [];
+
+var userFirst= prompt("First name");
+firstArr.push(userFirst);
+
+var userLast = ("Last name");
+lastArr.push(userLast);
+
+window.alert(userFirst[0] + " " + userLast[0] + firstArr.length + lastArr.length);
